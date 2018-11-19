e3084dba65e54aad4e93e1cc67f82088458fc15e
The Narrow <44857957+LeeTheNarrow@users.noreply.github.com>
2018-11-20 03:08:01 +0900

    Update 5_텍스트 상자.py

--- 5_텍스트 상자.py
+++ 5_텍스트 상자.py
@@ -7,21 +7,45 @@ class App(QWidget):
  
     def __init__(self):
         super().__init__()
-        self.title = '멧돌_선택박스'
-        self.left = 1200
-        self.top = 500
-        self.width = 320
-        self.height = 200
+        self.title = '멧돌_선택박스'	 	# 창 이름
+        self.left = 1200            	# 모니터 내 창 위치(x)
+        self.top = 500     				# 모니터 내 창 위치(y)					
+        self.width = 320				# 창 사이즈 (가로)
+        self.height = 200				# 창 사이즈 (세로)
         self.initUI()
  
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+		'''
+		QMessageBox.Cancel	
+		QMessageBox.Ok	
+		QMessageBox.Help
+		QMessageBox.Open	
+		QMessageBox.Save	
+		QMessageBox.SaveAll
+		QMessageBox.Discard	
+		QMessageBox.Close	
+		QMessageBox.Apply
+		QMessageBox.Reset	
+		QMessageBox.Yes	
+		QMessageBox.YesToAll
+		QMessageBox.No	
+		QMessageBox.NoToAll	
+		QMessageBox.NoButton
+		QMessageBox.RestoreDefaults	
+		QMessageBox.Abort	
+		QMessageBox.Retry
+		QMessageBox.Ignore
+		'''
         while True :
-            buttonReply = QMessageBox.question(self, self.title, "종료하실건가요..??", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            buttonReply = QMessageBox.question(self, self.title, "종료하실건가요..??", QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
             if buttonReply == QMessageBox.Yes:
                 print('화이팅.. 힘내세요..')
+                break
+            if buttonReply == QMessageBox.Cancel:
+                print('앗 실수;;')
                 break
             else:
                 print('다음번엔 종료를...;;')
