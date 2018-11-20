65d8f8451253e062fe7779ad0dbfded5c046b793
goldmango328 <42968884+goldmango328@users.noreply.github.com>
2018-11-20 14:19:15 +0900

    Update image_color_cluster.py

--- Image_Color_Cluster/image_color_cluster.py
+++ Image_Color_Cluster/image_color_cluster.py
@@ -37,7 +37,7 @@ def plot_colors(hist, centroids):
     return bar
 
 def image_color_cluster(image_path, k = 5):
-    image = cv2.imread(image_path)
+    image = cv2.imread(image_path,cv2.IMREAD_UNCHANGED) # alpha channel 을 가져오는 것
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = image.reshape((image.shape[0] * image.shape[1], 3))
     
@@ -52,14 +52,15 @@ def image_color_cluster(image_path, k = 5):
     plt.imshow(bar)
     plt.show()
 
-img = Image.open('chair.jpg')
+# Image Convert : JPEG -> PNG
+img = Image.open('chair.jpg') # 해당하는 이미지 
 img = img.convert("RGBA")
 datas = img.getdata()
 
-newData = []
+newData = [] # 이미지를 pixel 단위로 쪼개서 alpha를 확인하여 제거하는 과정
 for item in datas:
     if item[0] > 200 and item[1] > 200 and item[2] > 200: # image의 흰색 배경을 제거하는 코드
-        newData.append((item[0], item[1], item[2],0))
+        newData.append((item[0], item[1], item[2],0)) # image alpha channel-> 0으로 줄여서 아예 보이지 않게 하는 것
     else :
         newData.append(item)
 
@@ -67,9 +68,11 @@ img.putdata(newData)
 img.save('chair_2.png',"PNG")
     
 image_path ="chair_2.png"
+# image path를 이렇게 설정해도 실제로 코드에서 돌아갈 때는
+# 배경이 있는 이미지로 진행됨 -> 왜??????ㅜㅠㅜㅠㅜ
 
 #preview image
-image = mpimg.imread(image_path)
+image = mpimg.imread(image_path,cv2.IMREAD_UNCHANGED)
 plt.imshow(image)
 
 image_color_cluster(image_path)
