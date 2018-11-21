89a14fa5236ef3fc8c45ef5ca41b6992f3cbae81
The Narrow <44857957+LeeTheNarrow@users.noreply.github.com>
2018-11-22 03:52:36 +0900

    Update 씐박.py

--- 씐박.py
+++ 씐박.py
@@ -1,6 +1,8 @@
+# please 파일이 필요해요! 같이 다운받아서 실행시켜주세요!!
+
 import sys
 from PyQt5.QtWidgets import QWidget, QListWidget, QListWidgetItem, QLabel, QPushButton, QApplication
-
+import please
 
 class exampleWidget(QWidget):
     def __init__(self):
@@ -12,12 +14,12 @@ class exampleWidget(QWidget):
         listWidget = QListWidget(self)
         listWidget.itemDoubleClicked.connect(self.buildExamplePopup)
 
-        names = ["Jack", "Chris", "Joey", "Kim", "Duncan"]
+        names = ["박서영 바보", "박서영 멍청이", "박서영 똥개", "박서영 해삼", "박서영 말미잘"]
 
         for n in names:
             QListWidgetItem(n, listWidget)
 
-        self.setGeometry(100, 100, 100, 100)
+        self.setGeometry(1200, 500, 250, 180)
         self.show()
 
     @staticmethod
@@ -38,6 +40,8 @@ class examplePopup(QWidget):
 
     def initUI(self):
         lblName = QLabel(self.name, self)
+        please.play(self.name)
+        print("I'm here")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
