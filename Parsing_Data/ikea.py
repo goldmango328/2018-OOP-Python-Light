7a8a40ebf3e02279ed3b80af2e9e4df08037e66b
agafe0415 <43326669+agafe0415@users.noreply.github.com>
2018-11-26 21:59:31 +0900

    Add files via upload

--- Parsing_Data/ikea.py
+++ Parsing_Data/ikea.py
@@ -1,75 +1,153 @@
 import bs4
 import requests
-import re
+
 
 def get_html(url):
-   """
-   웹 사이트 주소를 입력 받아, html tag 를 읽어드려 반환한다.
-   :param url: parsing target web url
-   :return: html tag
-   """
-   response = requests.get(url)
-   response.raise_for_status()
-
-   return response.text
+    """
+    웹 사이트 주소를 입력 받아, html tag 를 읽어드려 반환한다.
+    :param url: parsing target web url
+    :return: html tag
+    """
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        return '제품 정보가 없습니다.'
+    else:
+        return response.text
 
 def get_url(data):
-        url_main = data.select('div.productLists div.gridRow div.threeColumn div.image a')
-        url = []
-        for i in url_main:
-            url.append("https://www.ikea.com/" + i.get('href'))
-        return url
+    url_main = data.select('div.productLists div.gridRow div.threeColumn div.image a')
+    url = []
+    for i in url_main:
+        url.append("https://www.ikea.com/" + i.get('href'))
+    return url
 
-def get_img(sub):
+class parsing_data:
+    def __init__(self, in_url):
+        self.html= get_html(in_url)
+        self.data = bs4.BeautifulSoup(self.html, 'html.parser')
+        self.image = []
+        self.name = []
+        self.price = []
+        self.size = []
+        self.url = get_url(self.data)
+
+    def get_img(self, sub):
         sub_main = sub.select('div.rightContent div.pipContainer div#leftMainContainer div.rightContentContainer img#productImg')
         sub_main = str(sub_main[0]).split(' ')
         for line in sub_main:
             if 'src' in line:
                 line = line.replace('"', '')
                 line = line.replace('src=', '')
-                image.append(line)
+                self.image.append(line)
+        return self.image
 
-def get_name(sub):
+    def get_name(self, sub):
         name_now = sub.select('div.addList div.rightInfoDiv h1 span.productName')
         for i in name_now:
             name_in = i.getText().strip()
-            name.append(name_in)
+            self.name.append(name_in)
+        return self.name
 
-def get_price(sub):
+    def get_price(self, sub):
         price_now = sub.select('div.addList div.rightInfoDiv div.priceContainer span.packagePrice')
         for i in price_now:
             price_in = i.getText().strip()
-            price_in = str(price_in).replace('\xa0','')
-            price.append(price_in)
-            #print(price_in)
-def get_size(sub):
-    size_now = sub.select('div#metric')
-    for i in size_now:
-        size_in = i.getText().strip()
-        size_in = str(size_in).split(' cm')
-        if len(size_in) < 3:
-            size.append('none size')
-        else:
-            size.append(size_in[0].split(': ')[1] + ' cm X ' + size_in[1].split(': ')[1] + ' cm X ' + size_in[2].split(': ')[1] + ' cm')
+            price_in = str(price_in).replace('\xa0', '')
+            self.price.append(price_in)
+        return self.price
 
+    def get_size(self, sub):
+        size_now = sub.select('div#metric')
+        for i in size_now:
+            size_in = i.getText().strip()
+            size_in = str(size_in).split(' cm')
+            print(size_in)
+            if len(size_in) < 3:
+                self.size.append('none size')
+            else:
+                self.size.append([size_in[0].split(': ')[1],size_in[1].split(': ')[1],size_in[2].split(': ')[1]])
+        return self.size
+
+    def print_all(self):
+        for i in self.url:
+            sub_html = get_html(i)
+            if sub_html == '제품 정보가 없습니다.':
+                pass
+            else:
+                sub = bs4.BeautifulSoup(sub_html, 'html.parser')
+                self.get_name(sub)
+                self.get_price(sub)
+                self.get_size(sub)
+                self.get_img(sub)
+        return self.url, self.name, self.price, self.image, self.size
+
+class chair(parsing_data):
+    def get_size(self,sub):
+        size_now = sub.select('div#metric')
+        for i in size_now:
+            size_in = i.getText().strip()
+            size_in = str(size_in).split(' cm')
+            print(size_in)
+            if len(size_in) < 2:
+                self.size.append('none size')
+            else:
+                if '시험 중량: ' in size_in[0]:
+                    self.size.append([size_in[0].split(': ')[2], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
+                else:
+                    self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
+        return self.size
+
+class table(parsing_data):
+    def get_size(self, sub):
+        size_now = sub.select('div#metric')
+        for i in size_now:
+            size_in = i.getText().strip()
+            size_in = str(size_in).split(' cm')
+            print(size_in)
+            if len(size_in)<2:
+                self.size.append('none size')
+            elif len(size_in) == 3:
+                self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1]])
+            elif len(size_in) > 3:
+                self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
+        return self.size
+
+
+#class 처리
 #소파
-html = get_html('https://www.ikea.com/kr/ko/catalog/categories/departments/living_room/39130/')
-sofa = bs4.BeautifulSoup(html, 'html.parser')
-image = []
-name = []
-price = []
-size = []
+'''SOFA = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/living_room/39130/')
+url, name, price, image, size = SOFA.print_all()
+print(len(url), url)
+print(len(name), name)
+print(len(price), price)
+print(len(image), image)
+print(len(size), size)'''
 
-url = get_url(sofa)
-#링크를 타고 들어가서 사진, 이름, 가격, 사이즈 처리
-# 마지막에 사진을 넘겨줘서 사진의 대푯값 찾기
-cnt = 1
-for i in url:
-    sub_html = get_html(i)
-    sub = bs4.BeautifulSoup(sub_html, 'html.parser')
-    #get_img(sub)
-    #get_name(sub)
-    get_price(sub)
-    #get_size(sub)
+#의자
+'''CHAIR = chair('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/25219/')
+url, name, price, image, size = CHAIR.print_all()
+print(len(url), url)
+print(len(name), name)
+print(len(price), price)
+print(len(image), image)
+print(len(size), size)'''
 
-print(price)
+#책상
+'''TABLE = table('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/21825/')
+url, name, price, image, size = TABLE.print_all()
+print(len(url), url)
+print(len(name), name)
+print(len(price), price)
+print(len(image), image)
+print(len(size), size)'''
+
+#옷장
+'''CLOS = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/bedroom/10451/')
+url, name, price, image, size = CLOS.print_all()
+print(len(url), url)
+print(len(name), name)
+print(len(price), price)
+print(len(image), image)
+print(len(size), size)'''
