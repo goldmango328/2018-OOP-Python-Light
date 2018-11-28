47346dae5f8789818cca1d423056ce6759193ad1
The Narrow <44857957+LeeTheNarrow@users.noreply.github.com>
2018-11-28 23:11:59 +0900

    Update main_2.py

--- main_project/main_2.py
+++ main_project/main_2.py
@@ -7,8 +7,8 @@ class Checking(object):
 
     def make_checkbox(self, OtherWindow):    # 의자, 책상, 소파, 옷장
         OtherWindow.setObjectName("please help")
-        OtherWindow.resize(150,180)
-        OtherWindow.move(1100,400)
+        OtherWindow.resize(150,250)
+        OtherWindow.move(600,400)
         self.centralwidget = QWidget(OtherWindow)
         self.centralwidget.setObjectName("hi, I'm Baymax!")
                 
@@ -28,10 +28,19 @@ class Checking(object):
         self.checkbox4.setGeometry(QRect(50,110,150,30))
         self.checkbox4.stateChanged.connect(self.state)
 
+        self.next = QPushButton(self.centralwidget)
+        self.next.setGeometry(QRect(50,180,50,30))
+        font = QFont()
+        font.setBold(True)
+        self.next.setFont(font)
+        self.next.setObjectName('congratulation')
+
         OtherWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(OtherWindow)
         self.retranslateUi(OtherWindow)
         QMetaObject.connectSlotsByName(OtherWindow)
+
+        
 
     def retranslateUi(self, OtherWindow):
         translate = QCoreApplication.translate
@@ -40,6 +49,7 @@ class Checking(object):
         self.checkbox2.setText(translate("OtherWindow",'책상'))
         self.checkbox3.setText(translate("OtherWindow",'소파'))
         self.checkbox4.setText(translate("OtherWindow",'옷장'))
+        self.next.setText(translate("OtherWindow",'완료'))
                            
     def state(self):
         msg = ""
