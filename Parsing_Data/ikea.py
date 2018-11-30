4f12959c5e97b9295d8b8c19c6b730c48d15507d
agafe0415 <43326669+agafe0415@users.noreply.github.com>
2018-11-30 12:04:39 +0900

    Add files via upload

--- Parsing_Data/ikea.py
+++ Parsing_Data/ikea.py
@@ -1,5 +1,6 @@
 import bs4
 import requests
+import image_percent_plot
 
 
 def get_html(url):
@@ -24,6 +25,7 @@ def get_url(data):
     return url
 
 class parsing_data:
+    global cnt
     def __init__(self, in_url):
         self.html= get_html(in_url)
         self.data = bs4.BeautifulSoup(self.html, 'html.parser')
@@ -31,6 +33,7 @@ class parsing_data:
         self.name = []
         self.price = []
         self.size = []
+        self.color = []
         self.url = get_url(self.data)
 
     def get_img(self, sub):
@@ -40,35 +43,40 @@ class parsing_data:
             if 'src' in line:
                 line = line.replace('"', '')
                 line = line.replace('src=', '')
-                self.image.append(line)
-        return self.image
+                now_img = line
+        return now_img
 
     def get_name(self, sub):
         name_now = sub.select('div.addList div.rightInfoDiv h1 span.productName')
-        for i in name_now:
-            name_in = i.getText().strip()
-            self.name.append(name_in)
+        #print(name_now)
+        name_in = name_now[0].getText().strip()
+        print(name_in)
+        self.name.append(name_in)
         return self.name
 
     def get_price(self, sub):
         price_now = sub.select('div.addList div.rightInfoDiv div.priceContainer span.packagePrice')
-        for i in price_now:
-            price_in = i.getText().strip()
-            price_in = str(price_in).replace('\xa0', '')
-            self.price.append(price_in)
+        price_in = price_now[0].getText().strip()
+        price_in = price_in.replace('₩\xa0','')
+        print(price_in)
+        self.price.append(price_in)
         return self.price
 
     def get_size(self, sub):
         size_now = sub.select('div#metric')
-        for i in size_now:
-            size_in = i.getText().strip()
-            size_in = str(size_in).split(' cm')
-            print(size_in)
-            if len(size_in) < 3:
-                self.size.append('none size')
-            else:
-                self.size.append([size_in[0].split(': ')[1],size_in[1].split(': ')[1],size_in[2].split(': ')[1]])
+        size_in = size_now[0].getText().strip()
+        size_in = size_in.split(' cm')
+        if len(size_in) < 3:
+            self.size.append('none size')
+        else:
+            self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
         return self.size
+
+    def get_color(self, image_url):
+        image = image_percent_plot.URLtoImage(image_url)
+        image_info = image_percent_plot.image_color_cluster(image)
+        print(list(image_info.keys())[0])
+        return list(image_info.keys())[0]
 
     def print_all(self):
         for i in self.url:
@@ -77,77 +85,75 @@ class parsing_data:
                 pass
             else:
                 sub = bs4.BeautifulSoup(sub_html, 'html.parser')
-                self.get_name(sub)
-                self.get_price(sub)
-                self.get_size(sub)
-                self.get_img(sub)
-        return self.url, self.name, self.price, self.image, self.size
+                image = self.get_img(sub)
+                color = self.get_color('https://www.ikea.com'+ image)
+                if color in theme_pic_color:  #이 부분만 모듈로 수정하면 될 거같아요!
+                    print("Yes!")
+                    self.image.append(image)
+                    self.color.append(color)
+                    self.get_name(sub)
+                    self.get_price(sub)
+                    self.get_size(sub)
+                else:
+                    pass
+        return self.name, self.price, self.size, self.image
 
 class chair(parsing_data):
     def get_size(self,sub):
         size_now = sub.select('div#metric')
-        for i in size_now:
-            size_in = i.getText().strip()
-            size_in = str(size_in).split(' cm')
-            print(size_in)
-            if len(size_in) < 2:
-                self.size.append('none size')
+        size_in = size_now[0].getText().strip()
+        size_in = size_in.split(' cm')
+        if len(size_in) < 2:
+            self.size.append('none size')
+        else:
+            if '시험 중량: ' in size_in[0]:
+                self.size.append([size_in[0].split(': ')[2], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
             else:
-                if '시험 중량: ' in size_in[0]:
-                    self.size.append([size_in[0].split(': ')[2], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
-                else:
-                    self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
+                self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
         return self.size
 
 class table(parsing_data):
     def get_size(self, sub):
         size_now = sub.select('div#metric')
-        for i in size_now:
-            size_in = i.getText().strip()
-            size_in = str(size_in).split(' cm')
-            print(size_in)
-            if len(size_in)<2:
-                self.size.append('none size')
-            elif len(size_in) == 3:
-                self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1]])
-            elif len(size_in) > 3:
-                self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
+        size_in = size_now[0].getText().strip()
+        size_in = size_in.split(' cm')
+        print(size_in)
+        if len(size_in)<2:
+            self.size.append('none size')
+        elif len(size_in) == 3:
+            self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1]])
+        elif len(size_in) > 3:
+            self.size.append([size_in[0].split(': ')[1], size_in[1].split(': ')[1], size_in[2].split(': ')[1]])
         return self.size
 
 
+
+theme_pic_url = 'https://images.homify.com/c_fill,f_auto,q_auto:eco,w_980/v1490248980/p/photo/image/1918911/IMG_6320.jpg'
+theme_pic = image_percent_plot.URLtoImage(theme_pic_url)
+theme_pic_color = image_percent_plot.image_color_cluster(theme_pic)
+print(theme_pic_color.getkeys())
 #class 처리
 #소파
-'''SOFA = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/living_room/39130/')
-url, name, price, image, size = SOFA.print_all()
-print(len(url), url)
-print(len(name), name)
-print(len(price), price)
-print(len(image), image)
-print(len(size), size)'''
+def sofa_data():
+    SOFA = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/living_room/39130/')
+    SOFA.print_all()
+
 
 #의자
-'''CHAIR = chair('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/25219/')
-url, name, price, image, size = CHAIR.print_all()
-print(len(url), url)
-print(len(name), name)
-print(len(price), price)
-print(len(image), image)
-print(len(size), size)'''
+def chair_data():
+    CHAIR = chair('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/25219/')
+    CHAIR.print_all()
+
 
 #책상
-'''TABLE = table('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/21825/')
-url, name, price, image, size = TABLE.print_all()
-print(len(url), url)
-print(len(name), name)
-print(len(price), price)
-print(len(image), image)
-print(len(size), size)'''
+def table_data():
+    TABLE = table('https://www.ikea.com/kr/ko/catalog/categories/departments/dining/21825/')
+    TABLE.print_all()
+
 
 #옷장
-'''CLOS = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/bedroom/10451/')
-url, name, price, image, size = CLOS.print_all()
-print(len(url), url)
-print(len(name), name)
-print(len(price), price)
-print(len(image), image)
-print(len(size), size)'''
+def closet_data():
+    CLOS = parsing_data('https://www.ikea.com/kr/ko/catalog/categories/departments/bedroom/10451/')
+    CLOS.print_all()
+
+sofa_data()
